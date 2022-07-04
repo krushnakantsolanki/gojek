6ba7fbc2ba550394153cb604528e5e1651ffb326
krushnakantsolanki <krushnakant.solanki@thomsonreuters.com>
2022-07-04 17:59:18 +0530

    Unit test cases

--- app/src/test/java/com/gopay/mock/FakeApi.kt
+++ app/src/test/java/com/gopay/mock/FakeApi.kt
@@ -1,30 +1,31 @@
 package com.gopay.mock
 
-import com.gopay.network.WebService
+import com.gopay.network.PeopleApi
 import com.gopay.network.response.PeopleResponse
-import com.gopay.network.response.StarWarPeople
+import com.gopay.network.response.Peoples
 import java.io.IOException
 
-class FakeApi :WebService {
+class FakeApi : PeopleApi {
     var failureMsg: String? = null
-    private val peoplelist = mutableListOf<StarWarPeople>()
-    fun addPeople(people: StarWarPeople) {
+    private val peoplelist = mutableListOf<Peoples>()
+    fun addPeople(people: Peoples) {
         peoplelist.add(people)
     }
-    override suspend fun getStarWarPeople(page: Int, name: String): PeopleResponse {
-//        failureMsg?.let {
-//            throw IOException(it)
-//        }
-        val next=page+1
-        var previous:Int?=null
-        if(page>1)
-            previous=page-1;
+
+    override suspend fun getPeopleList(page: Int, name: String): PeopleResponse {
+        failureMsg?.let {
+            throw IOException(it)
+        }
+        val next = page + 1
+        var previous: Int? = null
+        if (page > 1)
+            previous = page - 1;
 
         return PeopleResponse(
-             count = 2,
-             next = "https://swapi.dev/api/people/?page=$next&name=",
-             previous = "https://swapi.dev/api/people/?page=$previous&name=",
-             results =peoplelist
+            count = 2,
+            next = "https://swapi.dev/api/people/?page=$next&name=",
+            previous = "https://swapi.dev/api/people/?page=$previous&name=",
+            results = peoplelist
             )
     }
 }
--- app/src/test/java/com/gopay/mock/PeopleFactory.kt
+++ app/src/test/java/com/gopay/mock/PeopleFactory.kt
@@ -1,19 +1,18 @@
 package com.gopay.mock
 
-import com.gopay.network.response.StarWarPeople
+import com.gopay.network.response.Peoples
 import java.util.concurrent.atomic.AtomicInteger
 
 class PeopleFactory {
 
     private val counter=AtomicInteger(0)
-    fun createPeopleFactory():StarWarPeople{
+    fun createPeopleFactory(): Peoples {
         val id = counter.incrementAndGet()
-        val people=StarWarPeople(
-          name="Luke Skywalker_$id",
-          height = "172$id",
-          mass = "77$id",
-          hair_color = "blond$id",
-          skin_color = "fair$id"
+        val people = Peoples(
+            name = "Luke Skywalker_$id",
+            height = "172$id",
+            mass = "77$id",
+            hair_color = "blond$id",
         )
 
         return people
